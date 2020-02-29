f6e3737a0375c6c5094871f1e61d0aa5c27c0c27
k03719010 <k03719010@gmail.com>
2020-02-29 12:51:25 +0900

    post#editを作成
    show.htmlからリンク　textareaに初期値表示

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'posts/index'
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
   get "posts/:id" => "posts#show"
 end
