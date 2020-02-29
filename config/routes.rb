b6bf47ff789c035a9e91edac3aab426f4656d0f1
k03719010 <k03719010@gmail.com>
2020-02-29 12:16:25 +0900

    新規投稿画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  
   get "/" => "home#top"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "about" => "home#about"
+
+  get 'posts/index'
+  get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
 end
