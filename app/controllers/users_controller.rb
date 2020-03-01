d70a4874556b705facdfe4d5ccf789f82f8350a3
k03719010 <k03719010@gmail.com>
2020-03-01 11:32:35 +0900

    signup機能にpassword入力追加・signup時にログインするように・signup失敗時に入力値を引き継ぐように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email],image_name: "default_user.png")
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "default_user.png",
+      password: params[:password]
+    )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
