ae9a5a19e57293cfa5af81d80a310bfb2902962e
k03719010 <k03719010@gmail.com>
2020-03-01 09:06:45 +0900

    userテーブルにimage_nameカラムを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_072311) do
+ActiveRecord::Schema.define(version: 2020_03_01_000413) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_072311) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_name"
   end
 
 end
