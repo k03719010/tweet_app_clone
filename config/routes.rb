a28e61affc77d10068c78c87d5a8d341ccd3f152
k03719010 <k03719010@gmail.com>
2020-03-03 15:26:58 +0900

    画像編集時の動作を変更
    その他progate準拠に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
-  
-  get 'users/index'
-  get "users/:id/edit" => "users#edit"
-  post "users/:id/update" => "users#update"
-  get "users/:id" => "users#show"
-  post "users/create" => "users#create"
-  get "signup" => "users#new"
-  get "/" => "home#top"
-  get "about" => "home#about"
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
 
-  get 'posts/index'
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+
+  get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
   post "posts/create" => "posts#create"
   get "posts/:id/edit" => "posts#edit"
-  get "posts/:id" => "posts#show"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
+  
+  get "/" => "home#top"
+  get "about" => "home#about"
 end
